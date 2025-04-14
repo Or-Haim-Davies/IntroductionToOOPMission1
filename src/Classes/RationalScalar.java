b067cfb3c31b68c03b063b2a98447a2b795804b8
tal1612006 <landat@post.bgu.ac.il>
2025-04-14 14:57:22 +0300

    Update RationalScalar.java

--- src/Classes/RationalScalar.java
+++ src/Classes/RationalScalar.java
@@ -4,11 +4,31 @@ public class RationalScalar implements Scalar {
     private int numerator;
     private int dominator;
 
+    public RationalScalar(int numerator,int dominator){
+
+         this.numerator=numerator;
+         this.dominator=dominator;
+    }
+
     public String toString(){
     }
     public Scalar add(Scalar s){
+        return s.add(this);
+    }
+
+    public Scalar add(RationalScalar s){
+        int numerator1=this.numerator;
+        int dominator1=this.dominator;
+        int numerator2=s.getNumerator();
+        int dominator2=s.getDominator();
+        return new RationalScalar(numerator1*dominator2+numerator2*dominator1,dominator1*dominator2);
+    }
+    public Scalar add(IntengerScalar s){
 
     }
+
+
+
     public Scalar mul(Scalar s){
 
     }
@@ -24,14 +44,33 @@ public class RationalScalar implements Scalar {
     public boolean equals(Object o){
 
     }
-    public Rational reduce(){
-        
-        int gcd=0;
-          while (dominator != 0){
-        remainder = numerator % dominator;
-        numerator = dominator;
-        dominator = remainder;
-     }
-        
-    return a
+    public RationalScalar reduce() {
+        int gcd = 0;
+        int remainder = 0;
+        int dominator = this.dominator;
+        int numerator = this.numerator;
+        while (dominator != 0) {
+            remainder = numerator % dominator;
+            numerator = dominator;
+            dominator = remainder;
+        }
+        return new RationalScalar(this.numerator/numerator,this.dominator/numerator);
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDominator() {
+        return dominator;
+    }
+
+    public void setDominator(int dominator) {
+        this.dominator = dominator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
 }
+
