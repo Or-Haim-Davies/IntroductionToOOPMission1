c1dc8c920b95cca65913f0752c5d2ef4f4944987
tal1612006 <landat@post.bgu.ac.il>
2025-04-14 14:57:51 +0300

    Update Scalar.java

--- src/Classes/Scalar.java
+++ src/Classes/Scalar.java
@@ -2,6 +2,8 @@ package Classes;
 
 public interface Scalar {
     public Scalar add(Scalar s);
+    public Scalar add(RationalScalar s);
+    public Scalar add(IntengerScalar s);
     public Scalar mul(Scalar s);
     public Scalar neg();
     public Scalar power(int exponent);
